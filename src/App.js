be56fcfc9d565e177f70f8ac2526ab19f6299538
thor-tor <michal.kabara@gmail.com>
2019-11-11 17:16:27 +0100

    hooks implementation

--- src/App.js
+++ src/App.js
@@ -1,79 +1,15 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './App.css'
-import MoviesData from "./components/MoviesData"
 import Movie from "./components/Movie"
-import axios from 'axios'
-import Newbutton from './components/Newbutton'
 
-class App extends Component{
+const App = () => {
 
-  constructor(){
-    super()
-    this.state = {
-      movies: MoviesData
-    }
-    this.addMovie = this.addMovie.bind(this)
-    this.removeMovie = this.removeMovie.bind(this)
-  }
-  
-  async componentDidMount() {
-    await axios.get('./components/MoviesData.js')
-    this.setState({movies: MoviesData})
-  }
+  return (
+    <div>
+            <Movie />
+    </div>
 
-  async addMovie(){
-
-    if(this.state.movies.length === 1){
-      return
-    }
-    
-    axios.put(`/recommendations/${this.state.movies[0].id}/accept`,{
-    })
-    .then(res => { console.log(res.data)})
-
-
-    this.setState((prevState) => ({
-      movies: prevState.movies.slice(1)
-    }))
-    console.log(this.state.movies)
-  }
-  
-      
-  removeMovie(){
-
-    if(this.state.movies.length === 1){
-      return
-    }
-
-    this.setState((prevState) => ({
-      movies: prevState.movies.slice(1)
-    }));
-    axios.put(`/recommendations/${this.state.movies[0].id}/reject`,{
-
-    })
-    .then(res => { console.log(res.data)})
-    console.log(this.state.movies)
-  }
-
-  
-
-  render(){
-
-  
-  const MovieComponents = this.state.movies.map(movie => <Movie key={movie.id} movie={movie}/>)
-
-
-
-    return (
-      <div className="App">
-          <Newbutton />
-            {MovieComponents[0]}
-            <button className="acceptButton" onClick={this.addMovie}> Accept</button>
-            <button className="rejectButton" onClick={this.removeMovie}> Reject</button>
-      </div>
     )
   }
-}  
-
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+const Movie = () => {
+
+    const [moviesList, setMoviesList] = useState([])
+    const [movieTitle, setMovieTilte] = useState("Titanic")
+    const [movieImage, setMovieImage] = useState("https://images-na.ssl-images-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY1000_CR0,0,677,1000_AL_.jpg")
+    const [movieSummary, setMovieSummary] = useState("Lorem Ipsum")
+    const [movieRating, setMovieRating] = useState("1")
+  
+    useEffect(() => {
+
+    },[])
 
 
 
-function Movie(props){
-        return(
-            <div className="movieContainer">
-                <h2>{props.movie.title}</h2>
-                <img alt="movie poster" className="moviePoster" src={props.movie.imageURL}></img>
-                <p>{props.movie.summary}</p>
-                <p>({props.movie.rating}/10)</p>
-            </div>
+    return(
+        <div className="movieContainer">
+            <h2>{movieTitle}</h2>
+            <img alt="movie poster" className="moviePoster" src={movieImage}/>
+            <p>{movieSummary}</p>
+            <p>({movieRating}/10)</p>
+
+            <button className="acceptButton"> Accept</button>
+            <button className="rejectButton"> Reject</button>
+        </div>
         )
     }
+
+
 
 export default Movie

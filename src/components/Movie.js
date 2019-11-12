8f3344ab4f38f26f1241ec93e2edc1465ae4cefe
thor-tor <michal.kabara@gmail.com>
2019-11-12 21:26:49 +0100

    test

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,32 +1,45 @@
 import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 const Movie = () => {
 
-    const [moviesList, setMoviesList] = useState([])
-    const [movieTitle, setMovieTilte] = useState("Titanic")
-    const [movieImage, setMovieImage] = useState("https://images-na.ssl-images-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY1000_CR0,0,677,1000_AL_.jpg")
-    const [movieSummary, setMovieSummary] = useState("Lorem Ipsum")
-    const [movieRating, setMovieRating] = useState("1")
-  
-    useEffect(() => {
+    // const moviesData = [...MoviesData]
 
+    const [moviesData, setMoviesData] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3004/movies')
+        .then(res => {
+          const movies = res.data;
+          setMoviesData(movies)
+        //   console.log(movies)
+        })
     },[])
 
+    const AcceptMovie = () => {
+        // setMoviesDetails([...moviesData], moviesData.shift())
+        // console.log(moviesData)
+    }
 
+    const RejectMovie = () => {
+        // setMoviesDetails([...moviesData], moviesData.shift())
+        // console.log(moviesData)
+    }
+
+    console.log(moviesData)
+    const movie = [...moviesData]
+    console.log(movie[0])
 
     return(
         <div className="movieContainer">
-            <h2>{movieTitle}</h2>
-            <img alt="movie poster" className="moviePoster" src={movieImage}/>
-            <p>{movieSummary}</p>
-            <p>({movieRating}/10)</p>
-
-            <button className="acceptButton"> Accept</button>
-            <button className="rejectButton"> Reject</button>
+<h2>{moviesData[0]['title']}</h2>
+            <img alt="movie poster" className="moviePoster" src=""/>
+            <p></p>
+            <p>(/10)</p>
+            <button className="acceptButton" onClick={AcceptMovie}> Accept</button>
+            <button className="rejectButton" onClick={RejectMovie}> Reject</button>
         </div>
         )
     }
-
-
 
 export default Movie

10d0ad257062f5f572f3ee49ef3ff980c2c91289
thor-tor <michal.kabara@gmail.com>
2019-11-19 21:02:05 +0100

    commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import {createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension'
+
 
 const store = createStore(moviesStore);
 window.store = store;

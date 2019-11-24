7e098a84acd820a487ab3dee79d1e387e7d31236
thor-tor <michal.kabara@gmail.com>
2019-11-24 20:33:42 +0100

    movies container fix

--- src/state/movies/components/MoviesContainer.js
+++ src/state/movies/components/MoviesContainer.js
@@ -3,12 +3,12 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getAllMovies } from '../duck/operations.js'
 
 const MoviesContainer = (props) => {
-  const movies = useSelector(state => state.movies)
+  const movies = useSelector(state => state)
   const dispatch = useDispatch()
 
   React.useEffect(() => {
     dispatch(getAllMovies())
-  },)
+  },[dispatch])
 
   return <ul>
     {movies.list.map(movie => <li>{movie}</li>)}
